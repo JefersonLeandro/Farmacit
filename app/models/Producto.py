424b5edf9ad011e5cc4aacb2be4ea7e86448ea49
Jeferson Leandro <jefersonleandrorodriguez@gmail.com>
2024-01-25 10:16:05 -0500

    la vista queda listando los productos sin imagenes

--- app/models/Producto.py
+++ app/models/Producto.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import relationship
 
 class Producto (db.Model):
     
+    __tablename__ = "Productos"
     idProducto = db.Column(db.Integer, primary_key=True)
     nombreProducto = db.Column(db.String(45), nullable=False)
     descripcionUnidad = db.Column(db.String(45), nullable=False)
-    descripcionGeneral = db.Column(db.Text, nullable=False)
+    descripcionProductoGeneral = db.Column(db.Text, nullable=False)
     stockProducto = db.Column(db.Integer, nullable=False)
     precioProducto = db.Column(db.Integer, nullable=False)
     idMarcaProducto = db.Column(db.Integer, db.ForeignKey('MarcaProducto.id'))
--- app/routes/index_routes.py
+++ app/routes/index_routes.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template
 from app.models.Producto import Producto
-# from app import db
+from app.models.MarcaProducto import MarcaProducto
+from app import db
 
 bp = Blueprint('inicio', __name__)
 
 @bp.route('/')
 def index():
     
-    # productos = Producto.query.all()
+    productos = Producto.query.all()
+    marcasProductos = MarcaProducto.query.all()
+    print(productos, "--iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii--")
     
-    return render_template('index.html' )    
+    return render_template('index.html', productos = productos, marcasProductos = marcasProductos )    
 
 
 
