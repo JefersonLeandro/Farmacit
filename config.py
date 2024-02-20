4e8620447996a1c0caebe594f94225a6d919b7dc
Jeferson Leandro <jefersonleandrorodriguez@gmail.com>
2024-02-20 18:44:15 -0500

    config

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/DbFarmacit'
     # Conexión local 
     
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id21904228_jeferson:1002723384Je.@localhost:21/id21904228_mysql'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id21904228_jeferson:1002723384Je.@mysql20.000webhost.com:21/id21904228_mysql'
 
- 
+     
     # Conexion en linea 
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:d5HaBfE6EDGCHCgFh62agBceGedeGbDE@roundhouse.proxy.rlwy.net:42758/ProyectoFormativoF'
     
