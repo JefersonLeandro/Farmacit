96ed90349d9a26fda7c987c014e802d5ee5c9ccc
Jeferson Leandro <jefersonleandrorodriguez@gmail.com>
2024-02-21 16:39:39 -0500

    se agrego la conexion en el archivo confing a railway

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/DbFarmacit'
     # Conexión local 
     
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id21904228_jeferson:1002723384Je.@files.000webhost.com/id21904228_mysql'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id21904228_jeferson:1002723384Je.@files.000webhost.com/id21904228_mysql'
 
     
      
     # Conexion en linea 
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:d5HaBfE6EDGCHCgFh62agBceGedeGbDE@roundhouse.proxy.rlwy.net:42758/ProyectoFormativoF'
-    
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cEf6g3afc6DgHh5gAfBeFgBeFe333aFh@monorail.proxy.rlwy.net:14502/dbFarmacit'
     #hacer el local
     SQLALCHEMY_TRACK_MODIFICATIONS = False
