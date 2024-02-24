cfa1cdf015f89412c8e5951a6c6bd014f91a1af4
Jeferson Leandro <jefersonleandrorodriguez@gmail.com>
2024-02-23 19:50:09 -0500

    config

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
 class Config:
     
-    # Conexión local  
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/DbFarmacit'
+    # # Conexión local  
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/DbFarmacit'
    
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id21904228_jeferson:1002723384Je.@files.000webhost.com/id21904228_mysql'
 
     
      
     # Conexion en linea 
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cEf6g3afc6DgHh5gAfBeFgBeFe333aFh@monorail.proxy.rlwy.net:14502/dbFarmacit'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cEf6g3afc6DgHh5gAfBeFgBeFe333aFh@monorail.proxy.rlwy.net:14502/dbFarmacit'
 
     # tareas
     # -proteger las rutas con login_required
