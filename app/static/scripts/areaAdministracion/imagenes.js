b82fe2e92bea50471e75ff4a217cd85555c36acc
Gourmt <jefersonleandrorodriguez@gmail.com>
2024-07-28 17:30:10 -0500

    se agrego la funcionalidad de las flechas a la vista de imagenes.

---
+++ app/static/scripts/areaAdministracion/imagenes.js
@@ -0,0 +1,27 @@
+
+
+// Seleccionar todos los elementos <li> dentro del contenedor con la id "listaProductos"
+let listaItems = document.querySelectorAll("#listaProductos li");
+let inputId = document.querySelector("#inputD");
+
+// Iterar sobre cada elemento <li> utilizando forEach
+listaItems.forEach(function(li) {
+// Agregar un manejador de clic para cada elemento <li>
+    li.addEventListener("click", function(event) {
+        
+        // Obtener el valor del elemento <span> dentro del elemento <li> clicado
+        let span = li.querySelector("#unSpan");
+    
+        inputId.value = span.textContent;
+    });
+});
+
+function mostrarMensaje(mensaje){
+
+    // Función para mostrar una alerta después de cargar completamente la página
+    window.onload = function() {
+        window.setTimeout(function(){
+            alert(mensaje);
+        }, 500); 
+    };//500mls
+}
